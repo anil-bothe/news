253f2001708b1a53c1dbef2e24531b7fd02a864a
anil-bothe <anil.bothe.patil@gmail.com>
2022-06-11 10:06:22 +0530

    learning for loop, if else in jinja pattern (jinja is template engine)

--- app/views.py
+++ app/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, "index.html")
+    context = {
+        "data": [
+            {
+                "name": "anil",
+                "age": 26
+            },
+            {
+                "name": "Sunil",
+                "age": 46
+            },
+            {
+                "name": "Sanju",
+                "age": 15
+            },
+        ]
+    }
+
+    return render(request, "index.html", context)
 
 def contact(request):
     return render(request, "contact.html")
